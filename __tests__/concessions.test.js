641191cd5d1c293739c9cac9acbcc8f0a558db96
anickacodes <anickalewis@pursuit.org>
2023-05-31 15:06:45 -0400

    Concessions Tests completed

--- __tests__/concessions.test.js
+++ __tests__/concessions.test.js
@@ -1,15 +1,50 @@
-const concessions = require("../data/concessions")
+const concessions = require("../data/concessions.js");
 
 const {
-    getConcessionByID,
-    calculateTotalFromIDs
-} = require ("../data/concessions")
+  getConcessionByID,
+  calculateTotalFromIDs,
+} = require("../src/concessions");
 
 describe("getConcessionByID()", () => {
-    test("it returns A concession object with a matching ID or `null` if no object is found.", () => {
-      const actual = getConcessionByID(concessions, id);
-      const expected = concession.id === id || null;
-      expect(actual).toBe(expected);
-    });
-  });
+  it ("should find concession object with a matching ID ", () => {
+    const id = "KzWBehRAD";
 
+    const expected = { id: "KzWBehRAD", name: "Small Soda", priceInCents: 699 };
+    const actual = getConcessionByID(concessions, id);
+
+    expect(actual).toEqual(expected);
+  });
+});
+
+    it("if no matching ID return null", () => {
+    const id = null
+    const actual = getConcessionByID(concessions,id)
+    expect(actual).toEqual(null)
+});
+
+describe ("calculateTotalfromIDs", () => {
+    const concessions = [
+        { id: "g9sZdG1hI", name: "Small Popcorn", priceInCents: 929 },
+        { id: "0Qs9Yp2NL", name: "Large Popcorn", priceInCents: 1029 },
+        { id: "KzWBehRAD", name: "Small Soda", priceInCents: 699 },
+        { id: "NEHAsbTYk", name: "Large Soda", priceInCents: 749 },
+        { id: "rNVCeVsri", name: "Candy", priceInCents: 569 },
+    ]
+
+ it ("should return total value of all concessions from the 'ids'", () => {
+       
+        const ids = ["0Qs9Yp2NL", "NEHAsbTYk", "rNVCeVsri"] ;
+        const actualTotal = calculateTotalFromIDs(concessions, ids);
+        const expectedTotal =  2347
+//1029 + 749 + 569 = 
+        expect(actualTotal).toEqual(expectedTotal);
+    });
+
+
+    it ("should return undefined if concessions doesnt exist", () => {
+    const ids = ["Z21KehRJD", "RD15ehPJD"]
+    const actualTotal = calculateTotalFromIDs(concessions, ids);
+    
+    expect(actualTotal).toBe(0);
+    });
+});
--- __tests__/tickets.test.js
+++ __tests__/tickets.test.js
@@ -4,5 +4,19 @@ const tickets = require("../data/tickets")
 const {
     getTicketByName,
     calculateTotalFromTicketNames
-} = require("../data/tickets") 
+} = require("../src/tickets") 
 
+describe('getTicketByName', () => {
+it ('what happens the name is gary', () => {
+expect (getTicketByName(tickets, 'gary')).toBe(null);
+});
+
+it ('what happens when the name is adult matinee', () => {
+
+expect(getTicketByName(tickets, 'Adult Matinee')).toStrictEqual({
+id: '6ha0u__54',
+name: 'Adult Matinee',
+priceInCents: 949,
+});
+});
+});
