7d7fe5de0d61bea017be47025aea3e78c6cd092a
christophersanchez <christophersanchez@pursuit.org>
2023-06-02 13:52:39 -0400

    Jest Lab

--- __tests__/concessions.test.js
+++ __tests__/concessions.test.js
@@ -2,3 +2,17 @@ const {
     getConcessionByID,
     calculateTotalFromIDs,
 } = require("../src/concessions");
+
+describe.only("getConcessionByID", () => {
+    test("getConcessionByID should return the concession object with a matching ID", () => {
+        const concessions = [
+            { id: "g9sZdG1hI", name: "Small Popcorn", priceInCents: 929 },
+            { id: "0Qs9Yp2NL", name: "Large Popcorn", priceInCents: 1029 },
+            { id: "KzWBehRAD", name: "Small Soda", priceInCents: 699 },
+            { id: "NEHAsbTYk", name: "Large Soda", priceInCents: 749 },
+            { id: "rNVCeVsri", name: "Candy", priceInCents: 569 },
+        ];
+        const result = getConcessionByID(concessions, "0Qs9Yp2NL");
+        expect(result).toEqual({ id: "0Qs9Yp2NL", name: "Large Popcorn", priceInCents: 1029 })
+    })
+})
--- __tests__/tickets.test.js
+++ __tests__/tickets.test.js
@@ -64,5 +64,23 @@ describe.only("getTicketByName", () => {
       priceInCents: 1069
     })
   });
+
+  const { getTicketByName } = require('../src/tickets');
+
+  test("getTicketByName should return the ticket object with a matching name", () => {
+    const tickets = [
+      { id: "6ha0u__54", name: "Adult Matinee", priceInCents: 949 },
+      { id: "dlb8ka9N_", name: "Senior Matinee", priceInCents: 839 },
+      { id: "FSxeEmcoJ", name: "Child Matinee", priceInCents: 669 },
+      { id: "uD_PoicNN", name: "Adult Regular", priceInCents: 1369 },
+      { id: "TQh2t6HWf", name: "Senior Regular", priceInCents: 1219 },
+      { id: "B8v5R2nVQ", name: "Child Regular", priceInCents: 1069 },
+    ];
+
+    const result = getTicketByName(tickets, "Senior Matinee");
+    expect(result).toEqual({ id: "dlb8ka9N_", name: "Senior Matinee", priceInCents: 839 });
+  });
+
+
 });
 
