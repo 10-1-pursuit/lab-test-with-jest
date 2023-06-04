f25ec73dca149ab1812ed6d7c3f8ccdf9823fcdc
Michael Kleemoff <michaelkleemoff2@gmail.com>
2023-06-04 14:21:30 -0400

    Finish 'getTicketByName' with 2 tests one hundred percent coverage.

--- __tests__/tickets.test.js
+++ __tests__/tickets.test.js
@@ -4,3 +4,41 @@ const {
 	getTicketByName,
 	calculateTotalFromTicketNames,
 } = require('../src/tickets');
+
+describe('getTicketByName()', () => {
+	it('returns a ticket object when given the name.', () => {
+		const input = 'Adult Regular';
+		const actual = getTicketByName(tickets, input);
+		const expected = {
+			id: 'uD_PoicNN',
+			name: 'Adult Regular',
+			priceInCents: 1369,
+		};
+		expect(actual).toEqual(expected);
+	});
+	it(`returns 'null' if no object is found`, () => {
+		const input = 'Child Special';
+		const actual = getTicketByName(tickets, input);
+		const expected = null;
+		expect(actual).toEqual(expected);
+	});
+});
+
+describe('calculateTotalFromTicketNames()', () => {
+	it('returns a ticket object when given the name.', () => {
+		const input = 'Adult Regular';
+		const actual = calculateTotalFromTicketNames(tickets, input);
+		const expected = {
+			id: 'uD_PoicNN',
+			name: 'Adult Regular',
+			priceInCents: 1369,
+		};
+		expect(actual).toEqual(expected);
+	});
+	it(`returns 'null' if no object is found`, () => {
+		const input = 'Child Special';
+		const actual = calculateTotalFromTicketNames(tickets, input);
+		const expected = null;
+		expect(actual).toEqual(expected);
+	});
+});
