16c38d51bd51bed05d6584e1c7cc684246a64d81
Natyka Callwood <Natykacallwood@pursuit.org>
2023-06-04 00:38:02 -0400

    Added tests for tickets.js, still need to add tests for concessions.js.

---
+++ _tests_/concessions.test.js
@@ -0,0 +1,27 @@
+const  {getConcessionByID, calculateTotalFromIDs} = require("../src/concessions");
+
+
+describe("getConcessionByID", () => {
+    it(" ", () => {
+      // test code
+    });
+  
+    it(" ", () => {
+      // test code
+    });
+  });
+
+  describe("calculateTotalFromIDs", () => {
+    it(" ", () => {
+      // test code
+    });
+  
+    it(" ", () => {
+      // test code
+    });
+  });
+
+
+
+
+
---
+++ _tests_/tickets.test.js
@@ -0,0 +1,26 @@
+const {getTicketByName, calculateTotalFromTicketNames} = require("../src/tickets.js");
+
+describe("getTicketByName", () => {
+    it("Should return a ticket object from given ticket name.", () => {
+      // test code
+    });
+  
+    it("Should return null if no object is found ", () => {
+      // test code
+    });
+  });
+
+  describe("calculateTotalFromTicketNames", () => {
+    it("Should calculate a total based on the given ticket names.", () => {
+      // test code
+    });
+  
+    it("Should calculate the total value of all tickets from the `names` array.", () => {
+      // test code
+    });
+  
+
+  it("Should return 0 if no ticket is in the array" , () => {
+    // test code
+    });
+});
