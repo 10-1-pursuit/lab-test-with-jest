35655e6c6031ed60241b63c8c63e5d32a3b2d8f1
Natyka Callwood <Natykacallwood@pursuit.org>
2023-06-05 21:20:30 -0400

    Completed jest test lab, I wrote ten tests and all ten tests are passing.

--- _tests_/concessions.test.js
+++ _tests_/concessions.test.js
@@ -1,26 +1,51 @@
-const  {getConcessionByID, calculateTotalFromIDs} = require("../src/concessions");
+const  {
+  getConcessionByID, 
+  calculateTotalFromIDs,
+} = require("../src/concessions");
+
+const concessions = require("../data/concessions");
 
 
 describe("getConcessionByID", () => {
-    it(" ", () => {
-      // test code
+    test("Should return a concession object by matching ID.", () => {
+      const id = "g9sZdG1hI";
+      const actual = getConcessionByID(concessions, id);
+      const expected = {id: "g9sZdG1hI", name: "Small Popcorn", priceInCents: 929};
+      expect(actual).toStrictEqual(expected);
+
     });
   
-    it(" ", () => {
-      // test code
+    test("Should return 'null' if no object is found.", () => {
+      const id =[]
+      const actual = getConcessionByID(concessions, id);
+      const expected = null;
+      expect(actual).toStrictEqual(expected);
     });
   });
 
-  describe("calculateTotalFromIDs", () => {
-    it(" ", () => {
-      // test code
+describe("calculateTotalFromIDs", () => {
+    it("Should calculate the total based on the given IDs.", () => {
+      const id = ["KzWBehRAD", "NEHAsbTYk"];
+      const actual = calculateTotalFromIDs(concessions, id);
+      const expected = 1448;
+      expect(actual).toStrictEqual(expected);
     });
   
-    it(" ", () => {
-      // test code
+  
+    it("Should calculate the total value of all concessions from the `ids` array.", () => {
+      const id =["g9sZdG1hI", "0Qs9Yp2NL", "KzWBehRAD", "NEHAsbTYk", "rNVCeVsri"]
+      const actual = calculateTotalFromIDs(concessions, id);
+      const expected = 3975;
+      expect(actual).toStrictEqual(expected);
+    });
+
+    it("Should return 0 if no id found.", () => {
+      const id = [];
+      const actual = calculateTotalFromIDs(concessions, id);
+      const expected = 0;
+      expect(actual).toStrictEqual(expected);
     });
   });
-
 
 
 
--- _tests_/tickets.test.js
+++ _tests_/tickets.test.js
@@ -1,26 +1,51 @@
-const {getTicketByName, calculateTotalFromTicketNames} = require("../src/tickets.js");
+const {
+    getTicketByName, 
+    calculateTotalFromTicketNames
+} = require("../src/tickets.js");
+
+const tickets = require("../data/tickets");
 
 describe("getTicketByName", () => {
     it("Should return a ticket object from given ticket name.", () => {
-      // test code
+        const name = "Child Matinee";
+        const actual = getTicketByName(tickets, name);
+        const expected =  { id: "FSxeEmcoJ", name: "Child Matinee", priceInCents: 669 };
+        expect(actual).toStrictEqual(expected);
+
     });
   
     it("Should return null if no object is found ", () => {
-      // test code
+        const name = "Children Regular";
+        const actual = getTicketByName(tickets, name);
+        const expected = null;
+        expect(actual).toBe(expected);
+
     });
   });
 
-  describe("calculateTotalFromTicketNames", () => {
+describe("calculateTotalFromTicketNames", () => {
     it("Should calculate a total based on the given ticket names.", () => {
-      // test code
+        const name = ["Adult Regular", "Child Regular"];
+        const actual = calculateTotalFromTicketNames(tickets, name);
+        const expected = 2438;
+        expect(actual).toBe(expected);
+
     });
   
     it("Should calculate the total value of all tickets from the `names` array.", () => {
-      // test code
+        const name = ["Adult Matinee", "Child Matinee", "Senior Matinee", "Adult Regular", "Child Regular", "Senior Regular"];
+        const actual = calculateTotalFromTicketNames(tickets, name);
+        const expected = 6114;
+        expect(actual).toBe(expected);
+
     });
   
 
   it("Should return 0 if no ticket is in the array" , () => {
-    // test code
+        const name = [];
+        const actual = calculateTotalFromTicketNames(tickets, name);
+        const expected = 0;
+        expect(actual).toBe(expected);
+
     });
 });
